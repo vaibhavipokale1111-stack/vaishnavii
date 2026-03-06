5126ff14b0606baf889a9334b5f0834700d5da99
vaibhavipokale1111-stack <vaibhavipokale1111@gmail.com>
2026-03-06 17:06:07 +0530

    AA

---
+++ Vaishnavi1/src/main/java/InfiniteLoops.java
@@ -0,0 +1,20 @@
+public class InfiniteLoops {
+    public static void main(String[] args) {
+
+
+        while(true){
+            System.out.println("Infinite loop using while");
+        }
+
+
+        //do{
+        // System.out.println("Infinite loop using do-while");
+        //  }while(true);
+
+
+        // for(;;){
+        // System.out.println("Infinite loop using for");
+        // }
+
+    }
+}
