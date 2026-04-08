efd3210251d5608fbc0d3bdb765d1964c27d631e
vaibhavipokale1111-stack <vaibhavipokale1111@gmail.com>
2026-04-08 05:56:25 +0530

    A

---
+++ Vaishnavi1/src/main/java/ForEachExample.java
@@ -0,0 +1,10 @@
+class ForEachExample {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 3, 5, 7, 2, 9};
+
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
