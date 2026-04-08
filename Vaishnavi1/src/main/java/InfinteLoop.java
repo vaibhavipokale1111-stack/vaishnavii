306235f9ab3c9d1b3e41b810ec06159b23801d2a
vaibhavipokale1111-stack <vaibhavipokale1111@gmail.com>
2026-04-08 05:48:29 +0530

    A

---
+++ Vaishnavi1/src/main/java/InfinteLoop.java
@@ -0,0 +1,14 @@
+public class InfinteLoop {
+    public static void main(String[] args) {
+          for(int i = 1; i > 0; i++) {
+          System.out.println(i);
+          }
+
+        for (int i = 10; i >= 1; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
+
